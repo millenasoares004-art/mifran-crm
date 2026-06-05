b5dbc1ea3b824958fafd9f084634b6827570f325
millenasoares004-art <millenasoares004@gmail.com>
2026-06-04 22:07:27 -0300

    Add server.js - Express API with MongoDB integration

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL);
+
+const Cliente = mongoose.model('Cliente', {
+  nome: String,
+  telefone: String,
+  servico: String,
+  status: String,
+});
+
+app.get('/', (req,res) => {
+  res.send('API MIFRAN ONLINE 🚀');
+});
+
+app.post('/clientes', async (req,res) => {
+  const cliente = await Cliente.create(req.body);
+  res.json(cliente);
+});
+
+app.get('/clientes', async (req,res) => {
+  const clientes = await Cliente.find();
+  res.json(clientes);
+});
+
+app.listen(3000, () => console.log('Servidor rodando'));
